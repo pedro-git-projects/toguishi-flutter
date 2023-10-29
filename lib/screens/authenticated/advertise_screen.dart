b234b807df73e42cd9abb567df46454fb4cfab50
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-10-28 22:31:53 -0300

    adding dependencies|creating screen routes

---
+++ lib/screens/authenticated/advertise_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdvertiseScreen extends StatefulWidget {
+  const AdvertiseScreen({super.key});
+
+  @override
+  State<AdvertiseScreen> createState() => _AdvertiseScreenState();
+}
+
+class _AdvertiseScreenState extends State<AdvertiseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
