cd3c7eea7c3a4165d7a4c15344a0a61295c9d628
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-10-30 21:29:44 -0300

    removing unused import

--- lib/screens/unauthenticated/login_screen.dart
+++ lib/screens/unauthenticated/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'package:toguishi/screens/unauthenticated/choice_screen.dart';
-import 'package:toguishi/screens/unauthenticated/register_screen.dart';
 import 'dart:convert';
 
 import 'package:toguishi/state/auth_provider.dart';
