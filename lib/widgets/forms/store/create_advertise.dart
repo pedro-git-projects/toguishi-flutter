cea25df105a55ea1b96ae6caa0fd99a8c92f2b59
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-11-01 17:22:30 -0300

    creating submit function

--- lib/widgets/forms/store/create_advertise.dart
+++ lib/widgets/forms/store/create_advertise.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toguishi/state/config_provider.dart';
+import 'package:toguishi/state/storage_service.dart';
+
+import 'package:http/http.dart' as http;
 
 class CreateAdvertiseForm extends StatefulWidget {
   const CreateAdvertiseForm({super.key});
@@ -13,6 +20,86 @@ class CreateAdvertiseFormState extends State<CreateAdvertiseForm> {
   final remuneracaoController = TextEditingController();
   final beneficiosController = TextEditingController();
   final experienciaController = TextEditingController();
+
+  void _showMessageDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text(''),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showErrorDialog(String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Erro'),
+          content: Text(errorMessage),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _submitAdvertisement() async {
+    String descricao = descricaoController.text;
+    String remuneracao = remuneracaoController.text;
+    String beneficios = beneficiosController.text;
+    String experiencia = experienciaController.text;
+    List<String> selectedOptions = this.selectedOptions;
+
+    final config = Provider.of<ConfigProvider>(context, listen: false);
+    final ip = config.ip;
+
+    final requestBody = jsonEncode({
+      "Descrição": descricao,
+      "Remuneração": remuneracao,
+      "Benefícios": beneficios,
+      "Experiência": experiencia,
+      "Selected Options": selectedOptions,
+    });
+
+    print(requestBody);
+
+    final token = await StorageService().read('token');
+
+    final response = await http.post(
+      Uri.parse('http://$ip:3000/advertisement'),
+      headers: {
+        'Cotent-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      },
+      body: requestBody,
+    );
+
+    if (response.statusCode == 201) {
+      final message = jsonDecode(response.body)['message'];
+      _showMessageDialog(message);
+    } else {
+      final errorMessage = jsonDecode(response.body)['message'];
+      _showErrorDialog(errorMessage);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,6 +166,10 @@ class CreateAdvertiseFormState extends State<CreateAdvertiseForm> {
               }
             });
           },
+        ),
+        ElevatedButton(
+          onPressed: _submitAdvertisement,
+          child: const Text("Criar Anúncio"),
         ),
       ],
     );
