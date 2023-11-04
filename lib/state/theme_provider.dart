eadef75d74ba805271a7ae74f528a56547811156
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-11-03 21:14:01 -0300

    fixing dark mode

--- lib/state/theme_provider.dart
+++ lib/state/theme_provider.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode _currentThemeMode = ThemeMode.system;
 
+  var brightness =
+      SchedulerBinding.instance.platformDispatcher.platformBrightness;
+
+  bool firstTime = true;
+
   ThemeMode get currentThemeMode => _currentThemeMode;
 
   void toggleTheme() {
-    _currentThemeMode =
-        _currentThemeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    if (firstTime) {
+      _currentThemeMode =
+          brightness == Brightness.light ? ThemeMode.dark : ThemeMode.light;
+      firstTime = false;
+    } else {
+      _currentThemeMode = _currentThemeMode == ThemeMode.light
+          ? ThemeMode.dark
+          : ThemeMode.light;
+    }
+
     notifyListeners();
   }
 }
